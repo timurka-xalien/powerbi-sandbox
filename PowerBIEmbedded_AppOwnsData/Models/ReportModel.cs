7b825380779a0362a17e03f8e6a6677bc38b00ca
Timur Karanda <atkaranda@hotmail.com>
2018-06-19 15:56:55 +0300

    Initial commit. Implemented switching reports dynamically.

---
+++ PowerBIEmbedded_AppOwnsData/Models/ReportModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class ReportModel
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string EmbedToken { get; set; }
+    }
+}
