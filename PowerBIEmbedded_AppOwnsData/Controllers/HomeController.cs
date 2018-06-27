6d9ed91f8b7e4a6cd862fcd643a3647817616c23
Timur Karanda <atkaranda@hotmail.com>
2018-06-27 13:47:34 +0300

    Added RLS

--- PowerBIEmbedded_AppOwnsData/Controllers/HomeController.cs
+++ PowerBIEmbedded_AppOwnsData/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace PowerBIEmbedded_AppOwnsData.Controllers
         private static readonly string ApiUrl = ConfigurationManager.AppSettings["apiUrl"];
         private static readonly string GroupId = ConfigurationManager.AppSettings["groupId"];
         private static readonly string ReportId = ConfigurationManager.AppSettings["reportId"];
+        private static readonly string ReportRlsId = ConfigurationManager.AppSettings["reportRlsId"];
 
         public ActionResult Index()
         {
@@ -137,6 +138,117 @@ namespace PowerBIEmbedded_AppOwnsData.Controllers
 
             return View(result);
         }
+
+        public async Task<ActionResult> EmbedReportRls(string username, string roles)
+        {
+            var result = new EmbedConfig();
+            try
+            {
+                result = new EmbedConfig { Username = username, Roles = roles };
+                var error = GetWebConfigErrors();
+                if (error != null)
+                {
+                    result.ErrorMessage = error;
+                    return View(result);
+                }
+
+                // Create a user password cradentials.
+                var credential = new UserPasswordCredential(Username, Password);
+
+                // Authenticate using created credentials
+                var authenticationContext = new AuthenticationContext(AuthorityUrl);
+                var authenticationResult = await authenticationContext.AcquireTokenAsync(ResourceUrl, ClientId, credential);
+
+                if (authenticationResult == null)
+                {
+                    result.ErrorMessage = "Authentication Failed.";
+                    return View(result);
+                }
+
+                var tokenCredentials = new TokenCredentials(authenticationResult.AccessToken, "Bearer");
+
+                // Create a Power BI Client object. It will be used to call Power BI APIs.
+                using (var client = new PowerBIClient(new Uri(ApiUrl), tokenCredentials))
+                {
+                    // Get a list of reports.
+                    var reports = await client.Reports.GetReportsInGroupAsync(GroupId);
+
+                    Report report;
+                    if (string.IsNullOrEmpty(ReportRlsId))
+                    {
+                        // Get the first report in the group.
+                        report = reports.Value.FirstOrDefault();
+                    }
+                    else
+                    {
+                        report = reports.Value.FirstOrDefault(r => r.Id == ReportRlsId);
+                    }
+
+                    result.Reports = reports.Value.Select(r => new ReportModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        EmbedUrl = r.EmbedUrl
+                    }).ToList();
+
+                    if (report == null)
+                    {
+                        result.ErrorMessage = "Group has no reports.";
+                        return View(result);
+                    }
+
+                    var datasets = await client.Datasets.GetDatasetByIdInGroupAsync(GroupId, report.DatasetId);
+                    result.IsEffectiveIdentityRequired = datasets.IsEffectiveIdentityRequired;
+                    result.IsEffectiveIdentityRolesRequired = datasets.IsEffectiveIdentityRolesRequired;
+                    GenerateTokenRequest generateTokenRequestParameters;
+
+                    // This is how you create embed token with effective identities
+                    if (!string.IsNullOrEmpty(username))
+                    {
+                        var rls = new EffectiveIdentity(username, new List<string> { report.DatasetId });
+                        if (!string.IsNullOrWhiteSpace(roles))
+                        {
+                            var rolesList = new List<string>();
+                            rolesList.AddRange(roles.Split(','));
+                            rls.Roles = rolesList;
+                        }
+                        // Generate Embed Token with effective identities.
+                        generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view", identities: new List<EffectiveIdentity> { rls });
+                    }
+                    else
+                    {
+                        // Generate Embed Token for reports without effective identities.
+                        generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+                    }
+
+                    var tokenResponse = await client.Reports.GenerateTokenInGroupAsync(GroupId, report.Id, generateTokenRequestParameters);
+
+                    if (tokenResponse == null)
+                    {
+                        result.ErrorMessage = "Failed to generate embed token.";
+                        return View(result);
+                    }
+
+                    // Generate Embed Configuration.
+                    result.EmbedToken = tokenResponse;
+                    result.EmbedUrl = report.EmbedUrl;
+                    result.Id = report.Id;
+
+                    return View(result);
+                }
+            }
+            catch (HttpOperationException exc)
+            {
+                result.ErrorMessage = string.Format("Status: {0} ({1})\r\nResponse: {2}\r\nRequestId: {3}", exc.Response.StatusCode, (int)exc.Response.StatusCode, exc.Response.Content, exc.Response.Headers["RequestId"].FirstOrDefault());
+            }
+            catch (Exception exc)
+            {
+                result.ErrorMessage = exc.ToString();
+            }
+
+            return View(result);
+        }
+
 
         public async Task<ContentResult> GetReportEmbedToken(string reportId)
         {
